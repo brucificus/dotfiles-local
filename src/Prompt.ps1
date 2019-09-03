1febb2347797f09efc565a0a3106953fec3090de
Bruce Markham <219281+brucificus@users.noreply.github.com>
2019-09-03 12:05:06 -0400

    🐛 Adds Missing `-AllowClobber` for `oh-my-posh` Installation
    Without this, the profile will fail complete initialization on a machine that doesn't yet have `oh-my-posh` installed.

--- src/Prompt.ps1
+++ src/Prompt.ps1
@@ -3,13 +3,13 @@ Import-Module "$PSScriptRoot\Modules.psm1" -DisableNameChecking
 Ensure-CustomFontInstallations
 
 # We can't rely on `Ensure-ModuleInstallation` to detect module presence in this case,
-# because `PANSIES` is a clobbering-installation
+# because `PANSIES` is a clobbering-only-installation
 if (-not (Get-Command 'Get-Gradient' -ErrorAction SilentlyContinue)) {
     Ensure-ModuleInstallation PANSIES -MinimumVersion 1.4.0-beta03 -MaximumVersion 1.4.0-beta03 -AllowPrerelease -AllowClobber
 }
 Import-Module PANSIES
 
-Ensure-ModuleInstallation oh-my-posh -MinimumVersion 2.0.311 -MaximumVersion 2.0.311
+Ensure-ModuleInstallation oh-my-posh -MinimumVersion 2.0.311 -MaximumVersion 2.0.311 -AllowClobber
 Import-Module oh-my-posh
 
 Set-Theme "$PSScriptRoot\OhMyPoshTheme\Brucificus.psm1"
