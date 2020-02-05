f646af0282fb12dcb6155d750fbfaf5a8c1c9c9c
Bruce Markham <219281+brucificus@users.noreply.github.com>
2020-02-05 10:17:12 -0500

    ✨ Adds Support for Multiple Destinations for `Split-GitHistory`
    Leverages Git octopus merging in order to split a single file into multiple files while preserving Git history.
    
    For more information about octopus merging, see: http://www.freblogg.com/2016/12/git-octopus-merge.html

--- src/Git.psm1
+++ src/Git.psm1
@@ -7,35 +7,57 @@ function Split-GitHistory {
         [string] $source,
 
         [Parameter(Position = 1, Mandatory = $true)]
-        [string] $destination
+        [string[]] $destination
     )
-    $ErrorActionPreference = "Stop"
 
-    [string] $currentOperation
-    
-    $currentOperation = "♻🖇 Splits Git History (1/4): 🔤 Renames Original File to Destination"
-    Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
-    git mv $source $destination
-    git commit -m $currentOperation
-    
-    $currentOperation = "♻🖇 Splits Git History (2/4): 📎 Sidelines Original File"
-    Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
-    $saved = $(git rev-parse HEAD)
-    git reset --hard HEAD^
-    git mv $source "$source-copy"
-    git commit -m $currentOperation
-    
-    $currentOperation = "♻🖇 Splits Git History (3/4): 🔀 Merges Divergent Commits"
-    Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
-    git merge $saved                   # This will generate conflicts
-    git commit -a -m $currentOperation # Trivially resolve conflicts like this
-    
-    $currentOperation = "♻🖇 Splits Git History (4/4): ◀ Restores Original File"
-    Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
-    git mv "$source-copy" $source
-    git commit -m $currentOperation
+    Begin {
+        $ErrorActionPreference = "Stop"
+    }
 
-    Write-Progress -Activity "Splitting Git History of File" -Completed
+    Process {
+        [string] $currentOperation
+        
+        [string[]] $commitsToMerge = @()
+        [int] $opNumberMax = 3 + $destination.Length
+        
+        [int] $opNumber = 1
+        $currentOperation = "♻🖇 Git History Split ($opNumber/$opNumberMax): 📎 Sidelines Original File"
+        Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
+        [string] $sourceCopy = $source + "-" + $(git rev-parse HEAD)
+        git mv $source $sourceCopy
+        git commit -m $currentOperation
+        $commitsToMerge += $(git rev-parse HEAD)
+        git reset --hard HEAD^
+    
+        [int] $destinationNumber = 1
+        foreach ($singleDestination in $destination) {
+            $opNumber += 1
+            $currentOperation = "♻🖇 Git History Split ($opNumber/$opNumberMax): 🔤 Renames Original File to Destination $destinationNumber of $($destination.Length)"
+            Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
+            git mv $source $destination
+            git commit -m $currentOperation
+            $commitsToMerge += $(git rev-parse HEAD)
+            git reset --hard HEAD^
+            $destinationNumber += 1
+        }
+    
+        $opNumber += 1
+        $currentOperation = "♻🖇 Git History Split ($opNumber/$opNumberMax): 🔀 Merges Divergent Commits"
+        Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
+        [string] $mergeStartCommit = $commitsToMerge | select -first 1
+        [string[]] $remainingCommitsToMerge = $commitsToMerge | select -skip 1
+        git reset --hard $mergeStartCommit
+        git merge @remainingCommitsToMerge # This will generate conflicts
+        git commit -a -m $currentOperation # Trivially resolve conflicts like this
+    
+        $opNumber += 1
+        $currentOperation = "♻🖇 Git History Split ($opNumber/$opNumberMax): ◀ Restores Original File"
+        Write-Progress -Activity "Splitting Git History of File" -CurrentOperation $currentOperation
+        git mv $sourceCopy $source
+        git commit -m $currentOperation
+    
+        Write-Progress -Activity "Splitting Git History of File" -Completed
+    }
 }
 
 
