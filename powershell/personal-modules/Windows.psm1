d4045e513172ce9d7467460a7fa7b73f69fe71a0
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-06-12 15:55:59 -0400

    🔥 Removes Relocated PowerShell Helpers
    These have been ported over to my `dotfiles` repo so that they can be used in more circumstances.

--- powershell/personal-modules/Windows.psm1
+++
@@ -1,7 +0,0 @@
-function Test-Elevation() {
-    Write-Debug "Testing elevation"
-    return [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-}
-
-
-Export-ModuleMember -Function "Test-Elevation"
--- powershell/profile_local.psm1
+++ powershell/profile_local.psm1
@@ -1,9 +1,7 @@
 Import-Module -Name $PSScriptRoot\personal-modules\PowershellProfileMaintenance.psm1 -DisableNameChecking
 Import-Module -Name $PSScriptRoot\personal-modules\AwsHelpers.psm1 -DisableNameChecking
 Import-Module -Name $PSScriptRoot\personal-modules\FusionLog.psm1 -DisableNameChecking
-Import-Module -Name $PSScriptRoot\personal-modules\Git.psm1 -DisableNameChecking
 Import-Module -Name $PSScriptRoot\personal-modules\WebHelpers.psm1 -DisableNameChecking
-Import-Module -Name $PSScriptRoot\personal-modules\EnvHelper.psm1 -DisableNameChecking
 Import-Module -Name $PSScriptRoot\personal-modules\CustomFonts.psm1 -DisableNameChecking
 
 $env:HOME = $env:USERPROFILE
