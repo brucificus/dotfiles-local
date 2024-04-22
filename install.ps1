d2897e4f0e7bd55302d18c67e4b9947a01f57241
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-04-22 14:34:39 -0400

    ✨ Improves Edge-Case Handling in Dotfiles Installer Scripts

--- install.ps1
+++ install.ps1
@@ -1,22 +1,66 @@
+#!/usr/bin/env pwsh
+#Requires -Modules @{ModuleName="poshy-lucidity";ModuleVersion="0.3.16"}
 $ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
 
-$CONFIG = "install.conf.yaml"
-$DOTBOT_DIR = "dotbot"
 
-$DOTBOT_BIN = "bin/dotbot"
-$BASEDIR = $PSScriptRoot
+function Write-TerminatingError([string] $message, [object] $targetObject = $null) {
+    $errorRecord = [System.Management.Automation.ErrorRecord]::new(
+        [System.Exception]::new($message),
+        'LocalDotfilesError',
+        'OperationStopped',
+        $targetObject
+    )
+    Write-Error $errorRecord
+    $PSCmdlet.ThrowTerminatingError($errorRecord)
+}
 
-Set-Location $BASEDIR
-git -C $DOTBOT_DIR submodule sync --quiet --recursive
-git submodule update --init --recursive $DOTBOT_DIR
-
-foreach ($PYTHON in ('python', 'python3', 'python2')) {
-    # Python redirects to Microsoft Store in Windows 10 when not installed
-    if (& { $ErrorActionPreference = "SilentlyContinue"
-            ![string]::IsNullOrEmpty((&$PYTHON -V))
-            $ErrorActionPreference = "Stop" }) {
-        &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
-        return
+if (-not (Test-LinkCapability)) {
+    if ($IsWindows) {
+        Write-TerminatingError "🖇️ This script requires filesystem link capabilities. Please run this script in an elevated PowerShell session, or enable Developer Mode in Windows Settings."
+    } else {
+        Write-TerminatingError "🖇️ This script requires filesystem link capabilities. Please run this script in an elevated PowerShell session."
     }
 }
-Write-Error "Error: Cannot find Python."
+
+Push-Location $PSScriptRoot
+trap {
+    Pop-Location
+}
+
+# Make sure dotbot is up-to-date.
+$dotbot_dir = "dotbot"
+git submodule update --quiet --init --force --checkout --depth 1 --recursive $dotbot_dir
+
+# folders_to_relink = @()
+# folders_to_relink | Where-Object { -not (Test-ReparsePoint $_) } | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+
+function run_dotbot {
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $config_file
+    )
+    foreach ($python in ('python', 'python3', 'python2')) {
+        # Python redirects to Microsoft Store in Windows 10 when not installed
+        if (& { $ErrorActionPreference = "SilentlyContinue"
+                ![string]::IsNullOrEmpty((&$python -V))
+                $ErrorActionPreference = "Stop" }) {
+            $python_bin = $python
+            break
+        }
+    }
+    if (-not $python_bin) {
+        Write-TerminatingError "💥 Cannot find Python."
+    }
+    [string] $ds = [System.IO.Path]::DirectorySeparatorChar
+    $dotbot_bin = "$PSScriptRoot${ds}${dotbot_dir}${ds}bin${ds}dotbot"
+    &$python $dotbot_bin -d $PSScriptRoot -c $config_file
+}
+
+# Execute dotbot.
+run_dotbot "install.conf.yaml"
+if ($IsWindows -and (Test-Path "install.win.conf.yaml" -ErrorAction SilentlyContinue)) {
+    run_dotbot "install.win.conf.yaml"
+} elseif ($IsLinux -and $Env:WSL_DISTRO_NAME -and (Test-Path "install.wsl.conf.yaml" -ErrorAction SilentlyContinue)) {
+    run_dotbot "install.wsl.conf.yaml"
+}
