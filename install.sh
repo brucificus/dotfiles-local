24263a546f271bdf29fb041b5a87967333e7d973
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-18 16:37:06 -0500

    🔀 Merges Upstream Changes & Fixes Git Submodules

--- install.sh
+++ install.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
-
 set -e
 
-CONFIG="install.conf.yaml"
-DOTBOT_DIR="dotbot"
 
-DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "${BASEDIR}"
+# `return $ret` is important for when trapping a SIGINT:
+#  The return status from the function is handled specially. If it is zero, the signal is
+#  assumed to have been handled, and execution continues normally. Otherwise, the shell
+#  will behave as interrupted except that the return status of the trap is retained.
+#  This means that for a CTRL+C, the trap needs to return the same exit status so that
+#  the shell actually exits what it's running.
+trap "
+    ret=\$?
+    unset BASEDIR CONFIG CONFIG_WSL DOTBOT_DIR DOTBOT_BIN
+    popd
+    return \$ret
+" EXIT INT QUIT
 
-cd "${BASEDIR}"
-git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
-git submodule update --init --recursive "${DOTBOT_DIR}"
+# Make sure dotbot is up-to-date.
+DOTBOT_DIR="dotbot"
+git submodule update --quiet --init --force --checkout --depth 1 --recursive "${DOTBOT_DIR}"
 
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+# Execute dotbot.
+CONFIG="install.conf.yaml"
+CONFIG_WSL="install.wsl.conf.yaml"
+DOTBOT_BIN="bin/dotbot"
+"${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+if [ -n "$WSL_DISTRO_NAME" ] && [ -f "$CONFIG_WSL" ]; then
+    "${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG_WSL}" "${@}"
+fi
