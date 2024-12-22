e029676941c4da378c2204d4dc36b324e39462b4
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-21 19:18:07 -0500

    ⚰️ Removes Deprecated Personal PowerShell Modules

--- powershell/personal-modules/WebHelpers.psm1
+++
@@ -1,8 +0,0 @@
-function google([string] $query) {
-    Write-Debug "Googling `"$query`""
-    $url = "http://google.com/#safe=active&q=" + [System.Uri]::EscapeUriString($query)
-    Start-Process $url
-}
-
-
-Export-ModuleMember -Function "google"
