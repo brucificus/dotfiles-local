7b12e731d6c27ac94ed8cbff00ce4db268200737
Bruce Markham <bruce.markham@starz.com>
2026-05-19 08:56:32 -0600

    🐛 Fixes Terminal Unicode Display & Pager Invocations

--- shell_local_after.ps1
+++ shell_local_after.ps1
@@ -10,3 +10,14 @@ Set-StrictMode -Version Latest
 # We *do* declare DOTFILES_LOCAL *here* because it's not (authoritatively) defined
 # in the upstream `dotfiles` PowerShell stack.
 Set-EnvVar -Process -Name "DOTFILES_LOCAL" -Value "~/.dotfiles_local" -SkipOverwrite
+
+# Set the code page to UTF-8 to avoid display/encoding issues with non-ASCII characters when piped through native Windows commands.
+# (e.g. git log output with emojis when pager is "more.com")
+chcp 65001 > $null
+
+# Fix the pager.
+# TODO: Port fixed slashes to upstream PowerShell code.
+if ($env:PAGER -like "C:\Windows\system32\more.com*") {
+    $env:PAGER = "C:\\Windows\\system32\\more.com"
+    $env:MORE = "/E"
+}
